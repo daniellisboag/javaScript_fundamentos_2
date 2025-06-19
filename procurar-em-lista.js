7bebe70a899f7a220c33dcd79c0e1f5751ca2c78
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-19 20:35:51 -0300

    Procurando em lista

--- procurar-em-lista.js
+++ procurar-em-lista.js
@@ -7,7 +7,7 @@ function exibeNomeENota(aluno) {
    if (lista[0].includes(aluno)) {
       const indice = lista[0].indexOf(aluno);
       const mediaAluno = lista[1][indice];
-   console.log(`${aluno} tem a média ${mediaAluno}`);
+      console.log(`${aluno} tem a média ${mediaAluno}`);
    } else {
       console.log('Aluno não encontrado!');
    }
