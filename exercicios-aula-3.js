b5b731e196fc18e6f4fb0ca62a5eab3807136b7e
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-22 21:50:34 -0300

    Exercícios da 3° aula

--- exercicios-aula-3.js
+++ exercicios-aula-3.js
@@ -29,7 +29,7 @@ function somaNumerosInteiros(array) {
 
 let lista = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 somaNumerosInteiros(lista);
-console.log('A soma dos elementos é:', somaElementos); */
+console.log('A soma dos elementos é:', somaElementos);
 
 // 4. Crie uma função que receba uma array de números inteiros e retorne o menor valor e o maior valor da array, no seguinte formato: 'o menor número é X e o maior número é Y'.
 let lista = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
@@ -49,8 +49,20 @@ function maiorMenor(array) {
 }
 
 maiorMenor(lista)
-console.log(`O maior número é ${maior} e o menor é ${menor}`);
+console.log(`O maior número é ${maior} e o menor é ${menor}`); */
 
 // 5. Crie um programa que utilize um laço for para percorrer uma array `const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14]` e exibir no console apenas os números pares contidos nesse array.
+const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
+
+function numerosPares(array) {
+   for (let valor of array) {
+      if (valor % 2 == 0) {
+         console.log(valor);
+      }
+   }
+}
+
+console.log('Números pares:');
+numerosPares(numeros);
 
 // 6. Crie um programa que calcule a média dos números presentes em um array utilizando um loop for.
