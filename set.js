9999b284cd61b18416b95b1a540f658d7aa8a2c3
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-25 20:37:11 -0300

    Removendo elementos com Set

---
+++ set.js
@@ -0,0 +1,8 @@
+const nomes = ['Ana', 'Clara', 'Maria', 'Maria', 'João', 'João'];
+
+// const nomesAtualizados = new Set(nomes);
+
+const listaNomesAtualizados = [...new Set(nomes)];
+
+console.log(nomes);
+console.log(listaNomesAtualizados);
