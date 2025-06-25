a7a775ac8d78cc156a9b08ec41da33b662f59655
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-24 21:15:06 -0300

    Update filtrar.js

--- filtrar.js
+++ filtrar.js
@@ -1,7 +1,7 @@
 const alunos = ['Ana', 'Marcos', 'Maria', 'Mauro'];
 const media = [7, 4.5, 8, 7.5];
 
-const reprovados = alunos.filter((aluno, indice) => {
+const reprovados = alunos.filter((_, indice) => {
    return media[indice] < 7;
 });
 
